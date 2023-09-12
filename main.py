3cae81fad92f1d88c6605edf219f8c969938e73f
CoboAr <cobo.arnold@gmail.com>
2023-09-12 13:45:42 -0400

    Website Portfolio

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template,request
+import requests
+import smtplib
+from flask_bootstrap import Bootstrap5
+import os
+
+app= Flask(__name__)
+Bootstrap5(app)
+app = Flask(__name__)
+# environment variables for email credentials
+MAIL_ADDRESS_SENDER = os.environ.get("EMAIL_OF_SENDER")
+MAIL_ADDRESS_RECEIVER = os.environ.get("EMAIL_OF_RECEIVER")
+MAIL_APP_PW = os.environ.get("PASSWORD_KEY")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        data = request.form
+        name = data['name']
+        email = data['email']
+        phone = data['phone']
+        message = data['message']
+        print (f"{name}\n{email}\n{phone}\n{message}")
+        send_email(name,email,phone,message)
+        return render_template ("index.html", msg_sent=True)
+    return render_template("index.html", msg_sent=False)
+
+
+#sending email function
+def send_email(name, email,phone, message):
+    with smtplib.SMTP ("smtp.gmail.com", 587) as connection:
+        connection.starttls ()
+        connection.login (user=MAIL_ADDRESS_SENDER, password=MAIL_APP_PW)
+        connection.sendmail (from_addr=MAIL_ADDRESS_SENDER,
+                             to_addrs=MAIL_ADDRESS_RECEIVER,
+                             msg=f"Subject: Data from form!\n\nName:{name}\nEmail: {email}\nPhone:{phone}\nMessage:{message}")
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8001)
